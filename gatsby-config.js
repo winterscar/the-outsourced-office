d0d3ddbb40d8515fd9a55002834d44a19300b4d7
Mathieu Pasquet <mathieu@pasquet.co>
2020-07-17 18:38:05 +0100

    Update fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,16 @@ module.exports = {
       options: {
         tailwindConfig: `tailwind.config.js`
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Port Lligat Slab`,
+          `Poppins` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
     }
   ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default () => (
     <section className="pt-20 md:pt-40">
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
-          <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
+          <h1 className="text-4xl lg:text-5xl xl:text-6xl leading-none">
             Giving you the time to do what you do best.
           </h1>
           <p className="text-xl lg:text-2xl mt-6 font-light">
