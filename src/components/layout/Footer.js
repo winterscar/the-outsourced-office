fa14fc4da605291911006acb818dafee00005584
Mathieu Pasquet <mathieu@pasquet.co>
2020-07-18 16:18:45 +0100

    Update linkedin

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,14 +4,14 @@ const Footer = () => (
   <footer className="container mx-auto py-16 px-3 mt-48 mb-8 text-gray-800">
     <div className="flex -mx-3">
       <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">About Us</h2>
+        <h2 className="text-lg font-semibold">Outsourced Office</h2>
         <p className="mt-5">Giving you the time to do what you do best.</p>
       </div>
       <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">Social Media</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://facebook.com/???">Linkedin</a>
+            <a href="https://linkedin.com/company/outsourced-office/">Linkedin</a>
           </li>
         </ul>
       </div>
